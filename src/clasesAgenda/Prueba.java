192b90f927ec390fbe10d116c2e588827e52beb0
MarioJ <MarioJ@DESKTOP-SUPAJQG>
2023-05-12 12:14:46 +0200

    prueba ordenador a ordenador

---
+++ src/clasesAgenda/Prueba.java
@@ -0,0 +1,13 @@
+package clasesAgenda;
+
+public class Prueba {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Esto es una prueba");
+		
+		
+	}
+
+}

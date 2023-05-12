8e919ce14d4bdb121f53ac2c4a78fd43b3c2228c
MarioJ <MarioJ@DESKTOP-SUPAJQG>
2023-05-12 12:29:05 +0200

    Ultimo Commit

--- src/clasesAgenda/Prueba.java
+++ src/clasesAgenda/Prueba.java
@@ -8,7 +8,7 @@ public class Prueba {
 		System.out.println("Esto es una prueba");
 		System.out.println("Prueba desde ordena del felador profesional archienemigo de las niñas");
 		System.out.println("SOy el que juega , THE GAME HAS CHANGED");
-		
+		System.out.println("Ultimo Commit");
 	}
 
 }
